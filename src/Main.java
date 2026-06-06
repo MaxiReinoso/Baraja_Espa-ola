1acf7dd748e137a553d1d0f0916be239bd4d7497
leof0307 <leofach006@gmail.com>
2026-06-06 19:47:05 -0300

    Main de las cartas
    modifique totalmente el main para que muestra todo lo que hay en el codigo

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,44 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
+        // Inicializamos los objetos del juego
+        Baraja baraja = new Baraja();
+        Mesa mesa = new Mesa();
+
+        // barajeamos
+        System.out.println("\n-> Barajando el mazo...");
+        baraja.barajar();
+        // recorremos la baraja
+        baraja.recorrerBaraja();
+
+        //vemos la carta siguiente disponible
+        baraja.sacarSiguiente();
+        baraja.recorrerBaraja();
+
+        // Creamos un par de jugadores de prueba y los sentamos en la mesa
+        Jugador j1 = new Jugador("Leoo");
+        Jugador j2 = new Jugador("Maxi dou");
+        Jugador j3 = new Jugador("Chadvier");
+        Jugador j4 = new Jugador("Lauti God");
+        mesa.agregarJugador(j1);
+        mesa.agregarJugador(j2);
+        mesa.agregarJugador(j3);
+        mesa.agregarJugador(j4);
+
+        System.out.println("REepartimos las cartas a todos los jugadores QUE esten en mesa");
+        // Repartimos de forma dinámica según lo que ingresó el usuario
+        baraja.repartirMano(mesa.getListaJugadores(), 4);
+
+        // • MOSTRAR LAS CARTAS QUE YA HAN SEAN REPARTIDAS (Historial)
+        System.out.println();
+        baraja.recorrerCartasSacadas();
+
+        // • MOSTRAR LAS CARTAS QUE PERMANECEN DISPONIBLES EN LA BARAJA
+        System.out.println();
+        baraja.recorrerBaraja();
+
+        // Consultamos la cantidad final
+        System.out.println("Las cartas que quedaron son: ");
+        baraja.recorrerBaraja();
     }
 }
