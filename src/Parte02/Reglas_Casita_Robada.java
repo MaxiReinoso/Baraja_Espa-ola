baf97a20f2554c33b4ebf387cc756b6f1d92d5a2
LauGomezCanovas <lautarolegogomez@gmail.com>
2026-06-17 18:22:32 -0300

    Cambio nombre a Reglas_Casita_Robada | Añado metodo "mostrarReglas()"

---
+++ src/Parte02/Reglas_Casita_Robada.java
@@ -0,0 +1,26 @@
+package Parte02;
+
+public class Reglas_Casita_Robada {
+
+    public void mostrarReglas(){
+        System.out.println("---- REGLAS | CASITA ROBADA ----\n");
+        System.out.println("--------------------------------\n");
+
+        System.out.println("\n---- OBJETIVO DEL JUEGO: ----");
+        System.out.println("Acumular la mayor cantidad de cartas posible.\n");
+        System.out.println("Gana el jugador que acumule mas cartas al final de la partida");
+
+        System.out.println("\n---- JUGADORES ----");
+        System.out.println("Pueden jugar un minimo 2 personas");
+
+        System.out.println("\n---- REPARTO ----");
+        System.out.println("Se reparten 3 cartas a cada jugador");
+        System.out.println("Se colocan 4 cartas boca arriba en la mesa (La casita).");
+        System.out.println("Cuando los jugadores se quedan sin cartas en la mano,");
+        System.out.println("el repartidor da 3 cartas nuevas a cada uno del mazo original.");
+
+        System.out.println("\n---- TURNO DE JUEGO ----");
+        System.out.println("En su turno, cada jugador debe jugar UNA carta de su mano.");
+    }
+
+}
