e315ea2c87657e8dd5aa5facb219502dee1edb7c
XavierPappalardo <xavierdampapp@gmail.com>
2026-06-19 14:50:39 -0300

    Avances con elsistema de turnos y el menú desplegado

--- src/Parte02/CasitaRobada.java
+++ src/Parte02/CasitaRobada.java
@@ -36,7 +36,31 @@ public class CasitaRobada extends Juego{
 
     public void turno(Jugador jugador){
 
-        
+        ArrayList<Cartas> cartasJugador = jugador.mazo;
+
+        Cartas primerCarta = cartasJugador.get(0);
+
+        int numeroPrimerCarta = primerCarta.getNumero();
+
+        System.out.println("La primer carta de su mazo es: " + numeroPrimerCarta);
+
+        this.menu(numeroPrimerCarta);
+
+    }
+
+    public void menu(int numeroPrimerCarta){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("¿Qué desea hacer a continuación?\n(Ingrese la letra correspondiente a la opción que desea elegir):");
+
+        System.out.println("");
+
+        System.out.println("Ingrese la opción que desee elegir:");
+
+        String opcionElegida = scanner.nextLine();
+
+
 
     }
 
