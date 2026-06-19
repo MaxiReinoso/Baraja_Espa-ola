6fa3ce892f8d151086892add6d95c17ac893dbb7
Maximiliano Reinoso <maximilianoreinoso105@gmail.com>
2026-06-19 15:47:08 -0300

    Mejoras en la base de la parte 1

--- src/Parte01/Jugador.java
+++ src/Parte01/Jugador.java
@@ -5,13 +5,16 @@ import java.util.ArrayList;
 public class Jugador {
     public String Nombre;
     public ArrayList<Cartas> mazo;
+    private ArrayList<Cartas> casita;
 
     public String getNombre() {
         return Nombre;
     }
+
     public Jugador(String nombre) {
         Nombre = nombre;
         this.mazo = new ArrayList<>();
+        this.casita = new ArrayList<>(); // Inicialización ordenada en el constructor
     }
     public void aniadirCarta(Cartas cartita){
         this.mazo.add(cartita);
@@ -19,5 +22,6 @@ public class Jugador {
     public void vaciarMazo() {
         this.mazo.clear();
     }
+    public ArrayList<Cartas> getCasita() {return this.casita;}
 
 }
--- src/Parte01/Mesa.java
+++ src/Parte01/Mesa.java
@@ -5,21 +5,17 @@ import Parte02.*;
 import java.util.ArrayList;
 
 public class Mesa {
-
     private ArrayList<Jugador> listaJugadores;
-
+    private ArrayList<Cartas> cartasEnSuelo;
     public Mesa() {
         this.listaJugadores = new ArrayList<>();
-    }
+        this.cartasEnSuelo = new ArrayList<>();}
+    public ArrayList<Jugador> getListaJugadores() {return listaJugadores;}
+    public ArrayList<Cartas> getCartasEnSuelo() {return this.cartasEnSuelo;}
 
     public void agregarJugador(Jugador jugador) {
         this.listaJugadores.add(jugador);
     }
-
-    public ArrayList<Jugador> getListaJugadores() {
-        return listaJugadores;
-    }
-
     public void devolverCartasABaraja(Baraja barajaDelJuego) {
         System.out.println("Recogiendo las cartas del juego");
 
